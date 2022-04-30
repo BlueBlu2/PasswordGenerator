4c746581811c3fba6d73d2cd931b2f15721ccf9b
BlueBlu2 <eng.abdalrhmn.mohamed@gmail.com>
2022-04-30 02:43:10 +0200

    BindingLevel0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  onButtonClick() {
+    this.password = 'My Password';
+  }
 }
