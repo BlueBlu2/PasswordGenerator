9d9b0d60bfd5d713a7676b15361d11cbe5fb939a
BlueBlu2 <eng.abdalrhmn.mohamed@gmail.com>
2022-05-01 01:32:25 +0200

    BindingLevel1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(value: string){
+    const pValue = parseInt(value);
+    !isNaN(pValue) ? this.length = pValue : this.length = 0;
+  }
+
   onButtonClick() {
     this.password = 'My Password';
+    console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
+    console.log(this.length);
+
   }
 }
